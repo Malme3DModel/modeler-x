4f335cbcc3801f2c3e4b42a090b94900e9bd03ee
笹澤ようすけ <sasaicco@gmail.com>
2025-06-02 16:36:50 +0900

    テンプレをCascadeStudioに変更

--- docs/template/components/pages/NotFound404.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound404() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl">404 Not Found</h1>
-    </div>
-  );
-}
